8dddd0537018c204b36c13c157c4171ec4c46869
Rahul Tiwari <gaynexe@gmail.com>
2023-07-26 18:00:43 +0530

    angular skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -1,18 +1,20 @@
 using API.Data;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors();
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
+builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 // Configure the HTTP request pipeline.
 app.MapControllers();
 
